302af2147e9f0f320aa120f5cc9d233acd4da51c
Chris Andrew <72550656+chrishughandrew@users.noreply.github.com>
2023-02-01 15:23:19 +0000

    finalise purchaseTickets method.

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,34 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
     
-    public TicketServiceImpl() {
+    private TicketPaymentService ticketPaymentService;
+    private SeatReservationService seatReservationService;
+
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService, SeatReservationService seatReservationService) {
         super();
+
+        this.seatReservationService = seatReservationService;
+        this.ticketPaymentService = ticketPaymentService;
+
     }
 
     /**
      * Should only have private methods other than the one below.
+     */
+
+    /** 
+     * Takes an account and a booking request, validates the the request to meet busniess rule reqs,
+     * then creates seat reservation and takes payment.
+     *
+     * @param accountId the account attempting to make the booking.
+     * @param ticketTypeRequests the collection of type requests which constitutes the whole request
+     * @exception InvalidPurchaseException indvalidates the request and advises which rule has been broken 
      */
 
     @Override
@@ -25,10 +43,12 @@ public class TicketServiceImpl implements TicketService {
         validateRequest(ticketTypeRequests);
 
         //sum up the ticket costs and make payment
+        int totalTicketCost = RequestCalculator.sumRequestCost(ticketTypeRequests);
+        this.ticketPaymentService.makePayment(accountId, totalTicketCost);
         
         //sum up the required seats and reserve
-
- 
+        int totalSeatsRequired =  RequestCalculator.sumOfRequiredSeatsFromRequest(ticketTypeRequests);
+        this.seatReservationService.reserveSeat(accountId, totalSeatsRequired);
     }
 
 
@@ -46,13 +66,5 @@ public class TicketServiceImpl implements TicketService {
         RequestValidator.ticketQuantityWithinMaxBounds(ticketTypeRequests);
         RequestValidator.infantsDoNotExceedAdults(ticketTypeRequests);
     }
-
-
-
-
-    
-
-
-    
 
 }
--- cinema-tickets-java/src/test/java/TicketServiceTests.java
+++ cinema-tickets-java/src/test/java/TicketServiceTests.java
@@ -2,14 +2,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 
 public class TicketServiceTests {
     
-    //setup the implemetation to test -  there must be a better way of doing this?
-    private TicketServiceImpl ticketService= new TicketServiceImpl();
+    //setup the implemetation to test - surely there must be a better way of doing this? find out.
+    private TicketServiceImpl ticketService= new TicketServiceImpl(new TicketPaymentServiceImpl(), new SeatReservationServiceImpl());
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
